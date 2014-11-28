96991c577f77316e4f056be7e6b4fecd8d036954
Sören Brinkmann <soeren.brinkmann@gmail.com>
2014-11-27 22:15:18 -0800

    foo.h: Add ARRAY_SIZE macro

--- include/foo.h
+++ include/foo.h
@@ -1,6 +1,8 @@
 #ifndef __LIBFOO_H__
 #define __LIBFOO_H__
 
+#define ARRAY_SIZE(x)	(sizeof(x) / sizeof((x)[0]))
+
 int foo_chkadd(int a, int b);
 unsigned int foo_chkuadd(unsigned int a, unsigned int b);
 char *foo_gets(void);
